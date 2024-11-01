cb0156a1d09e9ca1f5ab3864e65123f0953f9cca
castromaxi <mc.castro@alumno.etec.um.edu.ar>
2024-11-01 12:46:09 -0300

    productos agregados al backend

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const port = 3000;
 config();
 const mongoUri = process.env.MONGO_URI;
 
-// Conecta a MongoDB usando Mongoose
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error('Error connecting to MongoDB:', err));
@@ -20,7 +19,15 @@ const userSchema = new mongoose.Schema({
     password: { type: String, required: true }
 });
 
+const productSchema = new mongoose.Schema({
+    Product_name: {type: String, required: true},
+    Product_type: {type: String, required: true},
+    Product_stock: {type: Number, required: true},
+    Product_price: {type: Number, required: true}
+})
+
 const User = mongoose.model('User', userSchema);
+const Product=mongoose.model('Products', productSchema);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -29,6 +36,15 @@ app.get('/users', async (req, res) => {
     try {
         const users = await User.find();
         res.json(users);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+app.get('/products', async (req, res) => {
+    try {
+        const products = await Product.find();
+        res.json(products);
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
@@ -47,11 +63,34 @@ app.get('/users/:id', async (req, res) => {
     }
 });
 
+app.get('/products/:id', async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id);
+        if (product) {
+            res.json(product);
+        } else {
+            res.status(404).json({ message: 'Product not found' });
+        }
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
 app.post('/users', async (req, res) => {
     const user = new User(req.body);
     try {
         const newUser = await user.save();
         res.status(201).json(newUser);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+
+app.post('/products', async (req, res) => {
+    const product = new Product(req.body);
+    try {
+        const newProduct = await product.save();
+        res.status(201).json(newProduct);
     } catch (err) {
         res.status(400).json({ message: err.message });
     }
@@ -72,6 +111,21 @@ app.put('/users/:id', async (req, res) => {
     }
 });
 
+app.put('/products/:id', async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id);
+        if (product) {
+            Object.assign(product, req.body);
+            await product.save();
+            res.json(product);
+        } else {
+            res.status(404).json({ message: 'Product not found' });
+        }
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+
 app.delete('/users/:id', async (req, res) => {
     try {
         const user = await User.findById(req.params.id);
@@ -80,6 +134,20 @@ app.delete('/users/:id', async (req, res) => {
             res.json({ message: 'User deleted' });
         } else {
             res.status(404).json({ message: 'User not found' });
+        }
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+app.delete('/products/:id', async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id);
+        if (product) {
+            await product.remove();
+            res.json({ message: 'Product removed' });
+        } else {
+            res.status(404).json({ message: 'Product not found' });
         }
     } catch (err) {
         res.status(500).json({ message: err.message });
